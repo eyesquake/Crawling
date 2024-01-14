4e7c3c8db093eeea15dffb410c19ce9400e24a2a
jinii9 <jinii915@naver.com>
2024-01-15 03:24:41 +0900

    데이터 수집 구조 / 엑셀

--- everytime/everytime.py
+++ everytime/everytime.py
@@ -2,34 +2,91 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
 import time
-from search import search_post
+
+pd.set_option('display.max_columns', None)  # 모든 열 출력
+pd.set_option('display.width', None)  # 출력 너비 설정
+pd.set_option('display.max_colwidth', None)  # 각 열의 최대 너비 설정
 
 #########################[에브라타임 로그인]#########################
-# ChromeDriver 경로 설정
-webdriver_service = Service('/Users/gachonswacademy02/Desktop/Selenium/chromedriver-mac-x64/chromedriver')
-
-# ChromeOptions 객체 생성
+webdriver_service = Service('/Users/gachonswacademyo7/Documents/KAKAO/아이디어톤/셀레니움/chromedriver-mac-x64/chromedriver')
 options = Options()
-
-# WebDriver 객체 생성
 driver = webdriver.Chrome(service=webdriver_service, options=options)
 
-# 에브리타임 로그인 페이지 열기
 driver.get('https://everytime.kr/login')
 
-# 로그인 정보 입력 (아이디와 비밀번호를 교체해야 합니다)
-driver.find_element(By.NAME, 'id').send_keys('your_id')  # 아이디 입력
-driver.find_element(By.NAME, 'password').send_keys('your_password')  # 비밀번호 입력
+driver.find_element(By.NAME, 'id').send_keys('jinii915')  # 아이디 입력
+driver.find_element(By.NAME, 'password').send_keys('Everytime1!')  # 비밀번호 입력
 
-# 로그인 버튼 클릭
 driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]').click()
 
-# 게시글 검색
-search_post(driver)
+time.sleep(10)
 
+#########################[소프트웨어학과 정보 공유 페이지]#########################
+link = driver.find_element(By.LINK_TEXT, '소프트웨어학과 정보 공유')
+link.click()
 
-# 키 누르기 전까지 창 띄우도록
-# input("클릭")
+#########################[검색]#########################
+search_box = driver.find_element(By.NAME, 'keyword')  # 검색창 선택
+search_box.send_keys('카엔프')  # 검색창에 "엔터프라이즈" 입력
+search_box.send_keys(Keys.RETURN)  # Enter 키 입력
 
+time.sleep(10)
 
+#########################[검색결과 수집]#########################
+data = pd.DataFrame(columns=['제목', '내용', '댓글', '대댓글'])  # 데이터를 저장할 DataFrame 생성
+
+post_link_list = [post.get_attribute('href') for post in
+                  driver.find_elements(By.CSS_SELECTOR, 'article.list a.article')][:5]
+
+for post_link in post_link_list:
+    # 각 게시물에 접근
+    driver.get(post_link)
+    time.sleep(5)
+
+    # 제목, 내용
+    title = driver.find_element(By.CSS_SELECTOR, 'h2.large').text
+    content = driver.find_element(By.CSS_SELECTOR, 'p.large').text
+
+    # 댓글
+    ## 댓글 리스트 수집
+    comment_list = driver.find_elements(By.CSS_SELECTOR, 'article')
+    for comment in comment_list:
+        if "parent" in comment.get_attribute("class"):
+            parent_comment = comment.find_element(By.CSS_SELECTOR, 'p.large').text
+
+            # 새로운 행 생성
+            new_row = pd.DataFrame({
+                '제목': [title],
+                '내용': [content],
+                '댓글': [parent_comment],
+                '대댓글': ['']  # 대댓글 데이터 추가
+            })
+
+            # 기존 DataFrame에 새로운 행 추가
+            data = pd.concat([data, new_row], ignore_index=True)
+
+        elif "child" in comment.get_attribute("class"):
+            child_comment_content = comment.find_element(By.CSS_SELECTOR, 'p.large').text
+
+            # 새로운 행 생성
+            new_row = pd.DataFrame({
+                '제목': [title],
+                '내용': [content],
+                '댓글': [''],
+                '대댓글': [child_comment_content]  # 대댓글 데이터 추가
+            })
+
+            # 기존 DataFrame에 새로운 행 추가
+            data = pd.concat([data, new_row], ignore_index=True)
+
+    # 뒤로 가기를 실행하여 게시물 리스트 페이지로 돌아가기
+    driver.back()
+    time.sleep(10)
+
+print(data)
+# 엑셀 파일로 저장
+data.to_excel('data.xlsx', index=False)
--- everytime/search.py
+++ everytime/search.py
@@ -1,68 +1,83 @@
-from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
-import time
-
-def search_post(driver):
-    print("검색 페이지로 이동")
-    time.sleep(40) # 페이지가 로드될 때까지 기다리기
-
-    #########################[소프트웨어학과 정보 공유 페이지]#########################
-    # '소프트웨어학과 정보 공유' 링크 클릭
-    link = driver.find_element(By.LINK_TEXT, '소프트웨어학과 정보 공유')
-    link.click()
-
-    #########################[검색]#########################
-    # 검색창에 "카엔프" 입력
-    search_box = driver.find_element(By.NAME, 'keyword')  # 검색창 선택
-    search_box.send_keys('카엔프')  # 검색창에 "카엔프" 입력
-
-    # 검색 실행(Enter 키 입력)
-    from selenium.webdriver.common.keys import Keys
-    search_box.send_keys(Keys.RETURN)  # Enter 키 입력
-
-    time.sleep(10)
-    #########################[검색결과 수집]#########################
-    # 모든 페이지를 순회하도록 while문 실행
-    while True:
-        print("페이지 전환")
-        # 게시글 링크 리스트 수집
-        post_link_list = [post.get_attribute('href') for post in
-                          driver.find_elements(By.CSS_SELECTOR, 'article.list a.article')]
-
-        for post_link in post_link_list:
-            # 각 게시물에 접근
-            driver.get(post_link)
-            time.sleep(5)
-
-            # 제목, 내용
-            title = driver.find_element(By.CSS_SELECTOR, 'h2.large')
-            content = driver.find_element(By.CSS_SELECTOR, 'p.large')
-            print("제목: ", title.text)
-            print("내용: ", content.text)
-
-            # 댓글
-            ## 댓글 리스트 수집
-            comment_list = driver.find_elements(By.CSS_SELECTOR, 'article')
-            # parent_comment = ""
-            for comment in comment_list:
-                if "parent" in comment.get_attribute("class"):
-                    parent_comment = comment.find_element(By.CSS_SELECTOR, 'p.large').text
-                    print("댓글: ", parent_comment)
-
-                elif "child" in comment.get_attribute("class"):
-                    child_comment_content = comment.find_element(By.CSS_SELECTOR, 'p.large').text
-                    print("대댓글: ", child_comment_content)
-
-            # 뒤로 가기를 실행하여 게시물 리스트 페이지로 돌아가기
-            driver.back()
-            time.sleep(10)
-
-        # 다음 페이지로 이동
-        try:
-            # '다음 페이지' 버튼 클릭
-            next_button = driver.find_element(By.CSS_SELECTOR, 'a.next')
-            next_button.click()
-            time.sleep(5)
-        except NoSuchElementException:
-            print("모든 페이지 순회 완료")
-            break
+# from selenium.common import NoSuchElementException
+# from selenium.webdriver.common.by import By
+# import pandas as pd
+# import time
+#
+# def search_post(driver):
+#     print("검색 페이지로 이동")
+#     time.sleep(30) # 페이지가 로드될 때까지 기다리기
+#
+#     # 빈 DataFrame 생성
+#     data = pd.DataFrame(columns=['제목', '내용', '댓글'])
+#
+#     #########################[소프트웨어학과 정보 공유 페이지]#########################
+#     # '소프트웨어학과 정보 공유' 링크 클릭
+#     link = driver.find_element(By.LINK_TEXT, '소프트웨어학과 정보 공유')
+#     link.click()
+#
+#     #########################[검색]#########################
+#     # 검색창에 "카엔프" 입력
+#     search_box = driver.find_element(By.NAME, 'keyword')  # 검색창 선택
+#     search_box.send_keys('카엔프')  # 검색창에 "카엔프" 입력
+#
+#     # 검색 실행(Enter 키 입력)
+#     from selenium.webdriver.common.keys import Keys
+#     search_box.send_keys(Keys.RETURN)  # Enter 키 입력
+#
+#     time.sleep(10)
+#     #########################[검색결과 수집]#########################
+#     # 모든 페이지를 순회하도록 while문 실행
+#     # while True:
+#     #     print("페이지 전환")
+#         # 게시글 링크 리스트 수집
+#     post_link_list = [post.get_attribute('href') for post in
+#                       driver.find_elements(By.CSS_SELECTOR, 'article.list a.article')][:2]
+#
+#     for post_link in post_link_list:
+#         # 각 게시물에 접근
+#         driver.get(post_link)
+#         time.sleep(5)
+#
+#         # 제목, 내용
+#         title = driver.find_element(By.CSS_SELECTOR, 'h2.large')
+#         content = driver.find_element(By.CSS_SELECTOR, 'p.large')
+#         print("제목: ", title.text)
+#         print("내용: ", content.text)
+#
+#         # 댓글
+#         ## 댓글 리스트 수집
+#         comment_list = driver.find_elements(By.CSS_SELECTOR, 'article')
+#         comments = []
+#         for comment in comment_list:
+#             if "parent" in comment.get_attribute("class"):
+#                 parent_comment = comment.find_element(By.CSS_SELECTOR, 'p.large').text
+#                 print("댓글: ", parent_comment)
+#                 comments.append(parent_comment)
+#
+#             elif "child" in comment.get_attribute("class"):
+#                 child_comment_content = comment.find_element(By.CSS_SELECTOR, 'p.large').text
+#                 print("대댓글: ", child_comment_content)
+#                 comments.append(child_comment_content)
+#
+#         # 새로운 행 생성
+#         new_row = pd.DataFrame({'제목': [title.text], '내용': [content.text], '댓글': ['\n'.join(comments)]})
+#
+#         # 기존 DataFrame에 새로운 행 추가
+#         data = pd.concat([data, new_row], ignore_index=True)
+#         print(data)
+#         # 뒤로 가기를 실행하여 게시물 리스트 페이지로 돌아가기
+#         driver.back()
+#         time.sleep(10)
+#
+#         # 다음 페이지로 이동
+#         # try:
+#         #     # '다음 페이지' 버튼 클릭
+#         #     next_button = driver.find_element(By.CSS_SELECTOR, 'a.next')
+#         #     next_button.click()
+#         #     time.sleep(5)
+#         # except NoSuchElementException:
+#         #     print("모든 페이지 순회 완료")
+#         #     break
+#
+#     # 결과 DataFrame 반환
+#     return data
